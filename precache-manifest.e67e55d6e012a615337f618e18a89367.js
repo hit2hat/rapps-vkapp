893082bbebb245dae3069761137ec48e22f636dc
hit2hat <hit2hat@icloud.com>
2019-10-26 01:22:37 +0300

    Updates

---
+++ precache-manifest.e67e55d6e012a615337f618e18a89367.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ac3f97d5e622b8acefd25e38f3a54126",
+    "url": "/rapps-vkapp/index.html"
+  },
+  {
+    "revision": "4ad2e12409bfc5ad2712",
+    "url": "/rapps-vkapp/static/css/2.27cde75d.chunk.css"
+  },
+  {
+    "revision": "4ad2e12409bfc5ad2712",
+    "url": "/rapps-vkapp/static/js/2.16ee02da.chunk.js"
+  },
+  {
+    "revision": "c090661266a5af416a9d",
+    "url": "/rapps-vkapp/static/js/main.5bed4d3e.chunk.js"
+  },
+  {
+    "revision": "46e62b243d340f06a5ca",
+    "url": "/rapps-vkapp/static/js/runtime-main.259f9477.js"
+  }
+]);
